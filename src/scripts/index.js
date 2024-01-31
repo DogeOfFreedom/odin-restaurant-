273712dc242ef51a496e66e32a04c8883aa21260
DogeOfFreedom <jchensi2000@gmail.com>
2024-01-31 12:12:14 +0800

    initial dist commit

--- src/scripts/index.js
+++
@@ -1,9 +0,0 @@
-import "../style.css";
-import { createHomePage } from "./home_page.js";
-import "../fonts/Louis George Cafe.ttf";
-import "../fonts/LUMINA.otf";
-
-let home_btn = document.querySelector(".home_btn")
-home_btn.addEventListener("click", createHomePage);
-
-createHomePage();
